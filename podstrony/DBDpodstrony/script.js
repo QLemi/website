81f17e258748b4ec3dac51d971feeb82b9cab71d
QLemi <115163203+QLemi@users.noreply.github.com>
2022-10-23 19:33:57 +0200

    Add files via upload

--- podstrony/DBDpodstrony/script.js
+++ podstrony/DBDpodstrony/script.js
@@ -18,165 +18,120 @@ document.querySelector("#fioletowa").addEventListener('change',()=>{
 
 
 var ile=0;
-var addony=new Array(0,0,0,0,0,0,0,0,0);
+var addony=new Array(0,0,0,0,0,0,0,0,0,0,0,0,0,0);
 
-document.querySelector('#Butterfly-Tape').addEventListener('change',()=>{
+
+function zaznaczone(id,numertablicy){
     var sum=0;
-    for (var i = 0; i < 9; i++) {
+    for (var i = 0; i < 14; i++) {
         sum += addony[i];
     }
+    
     if(sum<2){
-        if(document.querySelector('#Butterfly-Tape').checked){
-            addony[0]=1;
+        if(document.querySelector(id).checked){
+            addony[numertablicy]=1;
         }else{
-            document.querySelector('#Butterfly-Tape').checked=false;
-            addony[0]=0;
+            document.querySelector(id).checked=false;
+            addony[numertablicy]=0;
         }
     }else{
-        document.querySelector('#Butterfly-Tape').checked=false;
-        addony[0]=0;
+        document.querySelector(id).checked=false;
+        addony[numertablicy]=0;
     }
+}
+{
+document.querySelector('#Rubber-Gloves').addEventListener('change',()=>{
+    zaznaczone('#Rubber-Gloves',0);
+})
+document.querySelector('#Butterfly-Tape').addEventListener('change',()=>{
+    zaznaczone('#Butterfly-Tape',1);
 })
 document.querySelector('#Bandages').addEventListener('change',()=>{
-    var sum=0;
-    for (var i = 0; i < 9; i++) {
-        sum += addony[i];
-    }
-    if(sum<2){
-        if(document.querySelector('#Bandages').checked){
-            addony[1]=1;
-        }else{
-            document.querySelector('#Bandages').checked=false;
-            addony[1]=0;
-        }
-    }else{
-        document.querySelector('#Bandages').checked=false;
-        addony[1]=0;
-    }
+    zaznaczone('#Bandages',2);
+})
+document.querySelector('#Sponge').addEventListener('change',()=>{
+    zaznaczone('#Sponge',3);
 })
 document.querySelector('#Self-Adherent-Wrap').addEventListener('change',()=>{
-    var sum=0;
-    for (var i = 0; i < 9; i++) {
-        sum += addony[i];
-    }
-    if(sum<2){
-        if(document.querySelector('#Self-Adherent-Wrap').checked){
-            addony[2]=1;
-        }else{
-            document.querySelector('#Self-Adherent-Wrap').checked=false;
-            addony[2]=0;
-        }
-    }else{
-        document.querySelector('#Self-Adherent-Wrap').checked=false;
-        addony[2]=0;
-    }
+    zaznaczone('#Self-Adherent-Wrap',4);
 })
-
 document.querySelector('#Needle').addEventListener('change',()=>{
-    var sum=0;
-    for (var i = 0; i < 9; i++) {
-        sum += addony[i];
-    }
-    if(sum<2){
-        if(document.querySelector('#Needle').checked){
-            addony[3]=1;
-        }else{
-            document.querySelector('#Needle').checked=false;
-            addony[3]=0;
-        }
-    }else{
-        document.querySelector('#Needle').checked=false;
-        addony[3]=0;
-    }
+    zaznaczone('#Needle',5);
 })
 document.querySelector('#Medical-Scissors').addEventListener('change',()=>{
-    var sum=0;
-    for (var i = 0; i < 9; i++) {
-        sum += addony[i];
-    }
-    if(sum<2){
-        if(document.querySelector('#Medical-Scissors').checked){
-            addony[4]=1;
-        }else{
-            document.querySelector('#Medical-Scissors').checked=false;
-            addony[4]=0;
-        }
-    }else{
-        document.querySelector('#Medical-Scissors').checked=false;
-        addony[4]=0;
-    }
+    zaznaczone('#Medical-Scissors',6);
 })
 document.querySelector('#Gauze-Roll').addEventListener('change',()=>{
-    var sum=0;
-    for (var i = 0; i < 9; i++) {
-        sum += addony[i];
-    }
-    if(sum<2){
-        if(document.querySelector('#Gauze-Roll').checked){
-            addony[5]=1;
-        }else{
-            document.querySelector('#Gauze-Roll').checked=false;
-            addony[5]=0;
-        }
-    }else{
-        document.querySelector('#Gauze-Roll').checked=false;
-        addony[5]=0;
-    }
+    zaznaczone('#Gauze-Roll',7);
 })
 document.querySelector('#Surgical-Suture').addEventListener('change',()=>{
-    var sum=0;
-    for (var i = 0; i < 9; i++) {
-        sum += addony[i];
-    }
-    if(sum<2){
-        if(document.querySelector('#Surgical-Suture').checked){
-            addony[6]=1;
-        }else{
-            document.querySelector('#Surgical-Suture').checked=false;
-            addony[6]=0;
-        }
-    }else{
-        document.querySelector('#Surgical-Suture').checked=false;
-        addony[6]=0;
-    }
+    zaznaczone('#Surgical-Suture',8); 
 })
 document.querySelector('#Gel-Dressings').addEventListener('change',()=>{
-    var sum=0;
-    for (var i = 0; i < 9; i++) {
-        sum += addony[i];
-    }
-    if(sum<2){
-        if(document.querySelector('#Gel-Dressings').checked){
-            addony[7]=1;
-        }else{
-            document.querySelector('#Gel-Dressings').checked=false;
-            addony[7]=0;
-        }
-    }else{
-        document.querySelector('#Gel-Dressings').checked=false;
-        addony[7]=0;
-    }
+    zaznaczone('#Gel-Dressings',9);
 })
 document.querySelector('#Abdominal-Dressing').addEventListener('change',()=>{
-    var sum=0;
-    for (var i = 0; i < 9; i++) {
-        sum += addony[i];
-    }
-    if(sum<2){
-        if(document.querySelector('#Abdominal-Dressing').checked){
-            addony[8]=1;
-        }else{
-            document.querySelector('#Abdominal-Dressing').checked=false;
-            addony[8]=0;
-        }
-    }else{
-        document.querySelector('#Abdominal-Dressing').checked=false;
-        addony[8]=0;
-    }
+    zaznaczone('#Abdominal-Dressing',10);
 })
-
+document.querySelector('#Anti-Haemorrhagic-Syringe').addEventListener('change',()=>{
+    zaznaczone('#Anti-Haemorrhagic-Syringe',11);
+})
+document.querySelector('#Styptic-Agent').addEventListener('change',()=>{
+    zaznaczone('#Styptic-Agent',12);
+})
+document.querySelector('#Refined-Serum').addEventListener('change',()=>{
+    zaznaczone('#Refined-Serum',13);
+})
+}
 var botanika=0;
 var street=0;
+var boon=0;
+var desperate=0;
+var resilience=0;
+var leader=0;
+var bnew=0;
+
+
+document.querySelector('#d1').addEventListener('change',()=>{
+    document.querySelector('#Desperate-Measures').checked=true;
+    desperate=1;
+    
+})
+document.querySelector('#d2').addEventListener('change',()=>{
+    document.querySelector('#Desperate-Measures').checked=true;
+    desperate=1;
+})
+document.querySelector('#d3').addEventListener('change',()=>{
+    document.querySelector('#Desperate-Measures').checked=true;
+    desperate=1;
+})
+document.querySelector('#d4').addEventListener('change',()=>{
+    document.querySelector('#Desperate-Measures').checked=true;
+    desperate=1;
+})
+
+
+
+document.querySelector('#s1').addEventListener('change',()=>{
+    document.querySelector('#streetwise').checked=true;
+    street=1;
+    
+})
+document.querySelector('#s2').addEventListener('change',()=>{
+    document.querySelector('#streetwise').checked=true;
+    street=1;
+})
+document.querySelector('#s3').addEventListener('change',()=>{
+    document.querySelector('#streetwise').checked=true;
+    street=1;
+})
+document.querySelector('#s4').addEventListener('change',()=>{
+    document.querySelector('#streetwise').checked=true;
+    street=1;
+})
+
+
+
 document.querySelector('#botanyKnowledge').addEventListener('change',()=>{
     if(document.querySelector('#botanyKnowledge').checked){
         botanika=1;
@@ -184,66 +139,137 @@ document.querySelector('#botanyKnowledge').addEventListener('change',()=>{
         botanika=0;
     }
 })
+document.querySelector('#resilience').addEventListener('change',()=>{
+    if(document.querySelector('#resilience').checked){
+        resilience=1;
+    }else{
+        resilience=0;
+    }
+})
+document.querySelector('#leader').addEventListener('change',()=>{
+    if(document.querySelector('#leader').checked){
+        leader=1;
+    }else{
+        leader=0;
+    }
+})
+document.querySelector('#better-than-new').addEventListener('change',()=>{
+    if(document.querySelector('#better-than-new').checked){
+        bnew=1;
+    }else{
+        bnew=0;
+    }
+})
 document.querySelector('#streetwise').addEventListener('change',()=>{
     if(document.querySelector('#streetwise').checked){
         street=1;
     }else{
         street=0;
+        for(i=1;i<5;i++)
+        document.querySelector('#s'+i).checked=false;
     }
 })
+document.querySelector('#boon').addEventListener('change',()=>{
+    if(document.querySelector('#boon').checked){
+        boon=1;
+    }else{
+        boon=0;
+    }
+})
+document.querySelector('#Desperate-Measures').addEventListener('change',()=>{
+    if(document.querySelector('#Desperate-Measures').checked){
+        desperate=1;
+    }else{
+        desperate=0;
+        for(i=1;i<5;i++)
+        document.querySelector('#d'+i).checked=false;
+    }
+})
+
+
+
+
 var ladunki=0;
 setInterval(function () {
     var czasl=0;
+    var czasla=0
     document.querySelector("#wynika").innerHTML = "";
     if(wybranaa=='s'){
         ladunki=16;
+        czasl=1;
+        czasla=1.25;
+        document.querySelector("#wynika").innerHTML = "Szara apteczka posiada <span class='zmiennak'>16 </span>ładunków<br>";
     }else if(wybranaa=='z'){
         ladunki=24;
+        czasl=1;
+        czasla=1.35;
+        document.querySelector("#wynika").innerHTML = "Żółta apteczka posiada <span class='zmiennak'>24 </span>ładunki<br>";
     }else if(wybranaa=='zi'){
         ladunki=16;
+        czasl=1.5;
+        czasla=1.5;
+        document.querySelector("#wynika").innerHTML = "Zielona apteczka posiada <span class='zmiennak'>16</span> ładunków<br>";
     }else if(wybranaa=='f'){
         ladunki=32;
+        czasl=1;
+        czasla=1.5;
+        document.querySelector("#wynika").innerHTML = "Fioletowa apteczka posiada <span class='zmiennak'>32</span> ładunki<br>";
     }else{
         document.querySelector("#wynika").innerHTML = "Nie wybrano apteczki";
         return 0;
     }
     if(addony[0]==1){
-        
+        czasl*=1.05;
+        czasla*=1.05;
     }
     if(addony[1]==1){
         ladunki+=8;
     }
     if(addony[2]==1){
         ladunki+=8;
+        czasl*=1.05;
+        czasla*=1.05;
     }
     if(addony[3]==1){
-        
+        czasl*=1.15;
+        czasla*=1.15;
     }
     if(addony[4]==1){
-        
+        czasl*=1.15;
+        czasla*=1.15;
     }
     if(addony[5]==1){
-        ladunki+=8;
+        ladunki+=12;
     }
     if(addony[6]==1){
-        
+        czasl*=1.15;
+        czasla*=1.15;
     }
     if(addony[7]==1){
-        ladunki+=8;
+        ladunki+=16;
     }
     if(addony[8]==1){
         ladunki-=8;
+        czasl*=1.25;
+        czasla*=1.25;
     }
+    
     if(botanika==1){
         ladunki=ladunki*0.8;
+        czasl*=1.5;
+        czasla*=1.5;
     }
     if(street==1){
         ladunki=ladunki*1.25;
     }
+    if(boon==1){
+        czasl*=1.5;
+        czasla*=1.5;
+    }
+    czasl=(16/czasl).toFixed(2);
+    czasla=(16/czasla).toFixed(2);
+    ilelecz=(ladunki/16);
     
-    
-    ilelecz=Math.trunc(ladunki/16);
-    
-    document.querySelector("#wynika").innerHTML = "Apteczka posiada: "+ladunki+" ładunki/ów. Wystarczy ona na "+ilelecz+" leczenie. Czas leczenia to: "+czasl+"s ";
+    document.querySelector("#wynika").innerHTML += "Z wybranymi addonami ma ich: <span class='zmiennak'>"+ladunki+"</span><br> Wystarczy ona na <span class='zmiennak'>"+ilelecz+"</span> leczenie/a.<br> Czas leczenia siebie to: "+czasl+"s<br>Czas leczenia altruistycznego to: "+czasla+"s";
 
 },100);
